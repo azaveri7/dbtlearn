f37b4b90d12c6964c379426734277dd362fa1997
azaveri7 <azaveri7@gmail.com>
2022-06-07 07:14:12 +0530

    sources code

--- models/src/src_reviews.sql
+++ models/src/src_reviews.sql
@@ -1,11 +1,11 @@
-WITH raw_reviews AS ( 
-    SELECT * FROM AIRBNB.RAW.RAW_REVIEWS 
+WITH raw_reviews AS (
+    SELECT * FROM {{ source('airbnb','reviews')}}
 )
 
-SELECT listing_id, 
-    date AS review_date, 
-    reviewer_name, 
-    comments as review_text, 
+SELECT listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments as review_text,
     sentiment as review_sentiment
 FROM
     raw_reviews
